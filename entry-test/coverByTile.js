6e44003073adb5ce164f34fd211128b83b3ff7d1
Vitaliy Normatov <vnormatov@gmail.com>
2017-03-25 13:08:59 +0200

    cover tiles added

---
+++ entry-test/coverByTile.js
@@ -0,0 +1,8 @@
+'use strick';
+function coverByTile(height, width, tileHeight){
+let rowTileAmount = Math.ceil(width /tileHeight);
+let columnTileAmount = Math.ceil(height /tileHeight)
+return rowTileAmount *columnTileAmount;
+}
+
+module.exports = coverByTile;
