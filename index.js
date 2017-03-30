c901dc4dd1842dd62eafb8294cf6dc7f7efd0c3c
normatovv <normatovv@edi.su>
2017-03-30 17:58:21 +0300

    added stones and few tests

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ let bubbleSort = require('./bubble-sort');
 let coverByTile = require('./entry-test/coverByTile.js');
 let watermelon = require('./entry-test/watermelon.js');
 let beautifulyear = require('./entry-test/beautifulyear.js');
-
+let stones = require('./entry-test/stones.js');
 
 let algolib = {
     bubbleSort,
     coverByTile,
     watermelon,
-    beautifulyear
+    beautifulyear,
+    stones
 }
 
 module.exports = algolib;
---
+++ test/stones-spec.js
@@ -0,0 +1,23 @@
+'use strick';
+
+const algolib = require('../index.js');
+
+describe.only('#stonesInLine', function(){
+
+	it('count quantity of stones for deleting if line size is 3 and  stones follow as RRG ', function(){
+		let quantity = algolib.stones.getMinStonesToDell(3, 'RRG');
+		quantity.should.be.eql(1);
+	});
+
+	it('count quantity of stones for deleting if line size is 5 and  stones follow as RRRRR ', function(){
+		let quantity = algolib.stones.getMinStonesToDell(5, 'RRRRR');
+		quantity.should.be.eql(4);
+	});
+
+	it('count quantity of stones for deleting if line size is 1 and  stones follow as B ', function(){
+		let quantity = algolib.stones.getMinStonesToDell(1, 'B');
+		quantity.should.be.eql(0);
+	});
+
+});
+
