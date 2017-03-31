e63f7cd72a278f9c5f2e3438c6935879a994c4ae
normatovv <normatovv@edi.su>
2017-03-31 11:07:20 +0300

    adedd stones feature and tests

--- entry-test/stones.js
+++ entry-test/stones.js
@@ -1,17 +1,16 @@
 'use strick';
 
 function getMinStonesToDell(numberOfStones, line) {
-    let count = 0;
     let stones = line.split('');
+    let count = 0;
     for (var i = 0; i < stones.length; i++) {
         for (var j = i + 1; j < stones.length; j++) {
             if (stones[i] == stones[j]) {
-                stones.splice(j, 1);
                 count++;
             }
+            break;
         }
     }
-
     return count;
 
 }
--- test/stones-spec.js
+++ test/stones-spec.js
@@ -9,14 +9,44 @@ describe.only('#stonesInLine', function () {
         quantity.should.be.eql(1);
     });
 
-    it('count quantity of stones for deleting if line size is 5 and  stones follow as RRRRR ', function () {
-        let quantity = algolib.stones.getMinStonesToDell(5, 'RRRRR');
-        quantity.should.be.eql(4);
+    it('count quantity of stones for deleting if line size is 2 and  stones follow as RR ', function () {
+        let quantity = algolib.stones.getMinStonesToDell(2, 'RR');
+        quantity.should.be.eql(1);
     });
 
     it('count quantity of stones for deleting if line size is 1 and  stones follow as B ', function () {
         let quantity = algolib.stones.getMinStonesToDell(1, 'B');
         quantity.should.be.eql(0);
+    });
+
+    it('count quantity of stones for deleting if line size is 3 and  stones follow as RRR ', function () {
+        let quantity = algolib.stones.getMinStonesToDell(3, 'RRR');
+        quantity.should.be.eql(2);
+    });
+
+    it('count quantity of stones for deleting if line size is 4 and  stones follow as RRRR ', function () {
+        let quantity = algolib.stones.getMinStonesToDell(4, 'RRRR');
+        quantity.should.be.eql(3);
+    });
+
+    it('count quantity of stones for deleting if line size is 4 and  stones follow as BRBG ', function () {
+        let quantity = algolib.stones.getMinStonesToDell(4, 'BRBG');
+        quantity.should.be.eql(0);
+    });
+
+    it('count quantity of stones for deleting if line size is 2 and  stones follow as BG ', function () {
+        let quantity = algolib.stones.getMinStonesToDell(2, 'BRBG');
+        quantity.should.be.eql(0);
+    });
+
+    it('count quantity of stones for deleting if line size is 4 and  stones follow as RBBR ', function () {
+        let quantity = algolib.stones.getMinStonesToDell(4, 'RBBR');
+        quantity.should.be.eql(1);
+    });
+
+    it('count quantity of stones for deleting if line size is 50 and  stones follow as GRBGGRBRGRBGGBBBBBGGGBBBBRBRGBRRBRGBBBRBBRRGBGGGRB ', function () {
+        let quantity = algolib.stones.getMinStonesToDell(50, 'GRBGGRBRGRBGGBBBBBGGGBBBBRBRGBRRBRGBBBRBBRRGBGGGRB');
+        quantity.should.be.eql(18);
     });
 
 });
