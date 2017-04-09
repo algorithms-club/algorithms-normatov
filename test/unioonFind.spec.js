a64b9495c10dad2c13edd1b43c50e28e620fdc38
vnormatov@gmail.com <vnormatov@gmail.com>
2017-04-09 13:42:53 +0300

    refactored according to comments

--- test/unioonFind.spec.js
+++ test/unioonFind.spec.js
@@ -3,26 +3,26 @@ const algolib = require('../index.js');
 
 
 describe.only('#unionFind', function () {
-    it('should create proper data structure', function () {
+    it('#should create proper data structure', function () {
         let uf = new algolib.UnionFind();
         uf.connect.should.be.instanceOf(Function);
         uf.isConnected.should.be.instanceOf(Function);
     })
 
-    it('should #isConnected return false if not connected', function () {
+    it('#isConnected should return false if not connected', function () {
         let uf = new algolib.UnionFind();
         let isElementConnected = uf.isConnected(23, 2323);
         isElementConnected.should.be.false();
     })
 
-    it('should #isConnected return true if elements are connected', function () {
+    it('#isConnected should return true if elements are connected', function () {
         let uf = new algolib.UnionFind();
         uf.connect(3, 6);
         let isElementConnected = uf.isConnected(3, 6);
         isElementConnected.should.be.eql(true);
     })
 
-    it('should #isConnected return true if 3 elements are connected', function () {
+    it('#isConnected should return true if 3 elements are connected', function () {
         let uf = new algolib.UnionFind();
         uf.connect(3, 6);
         uf.connect(3, 10);
@@ -30,7 +30,7 @@ describe.only('#unionFind', function () {
         isElementConnected.should.be.eql(true);
     })
 
-    it('should #count return number of components if we have 2 components', function () {
+    it('#count should return number not only when we have 2 components', function () {
         let uf = new algolib.UnionFind();
         uf.connect(3, 6);
         uf.connect(8, 10);
@@ -38,14 +38,14 @@ describe.only('#unionFind', function () {
         numberOfComponents.should.be.eql(2);
     })
 
-    it('should #count return number of components if we have 1 component', function () {
+    it('#count should return number not only when we have 1 component', function () {
         let uf = new algolib.UnionFind();
         uf.connect(3, 6);
         let numberOfComponents = uf.count();
         numberOfComponents.should.be.eql(1);
     })
 
-    it('should #count return number of components if we have 3 components', function () {
+    it('#count should return number not only when we have 3 components', function () {
         let uf = new algolib.UnionFind();
         uf.connect(3, 6);
         uf.connect(7, 5);
@@ -54,21 +54,21 @@ describe.only('#unionFind', function () {
         numberOfComponents.should.be.eql(3);
     })
 
-    it('should #component return component name ', function () {
+    it('#component should return component name if component has 2 elements', function () {
         let uf = new algolib.UnionFind();
         uf.connect(1, 2);
         let nameOfComponent = uf.component(1);
         nameOfComponent.should.be.eql(2);
     })
 
-    it('should #component return undefined, because element not exist', function () {
+    it('#component should return undefined, because element not exist', function () {
         let uf = new algolib.UnionFind();
         uf.connect(1, 2);
         let nameOfComponent = uf.component(5);
         (nameOfComponent === undefined).should.be.true()
     })
 
-    it('should #component return component name', function () {
+    it('#component should return component name if component has more than 3 elements', function () {
         let uf = new algolib.UnionFind();
         uf.connect(1, 2);
         uf.connect(2, 3);
@@ -77,7 +77,7 @@ describe.only('#unionFind', function () {
         nameOfComponent.should.be.eql(5);
     })
 
-    it('should #component return component name', function () {
+    it('#component should return component name if component has more than 2 elements', function () {
         let uf = new algolib.UnionFind();
         uf.connect(1, 2);
         uf.connect(2, 3);
