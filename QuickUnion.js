063b605670721d5255ea9d256b7fa292d159f71c
Vitaliy Normatov <vnormatov@gmail.com>
2017-04-10 22:47:29 +0300

    added methods count() component(a) end tests for it

--- QuickUnion.js
+++ QuickUnion.js
@@ -8,29 +8,20 @@ class QuickUnion {
     }
 
     connect(a, b) {
-
         if (this.isConnected(a, b)) return;
-        if (isNaN(this._elements[a])) {
-            this._elements[a] = a;
-            this._numberElementsInComponent[a] = 1;
-        }
-        if (isNaN(this._elements[b])) {
-            this._elements[b] = b;
-            this._numberElementsInComponent[b] = 1;
-        }
+
+        if (isNaN(this._elements[a])) this._elements[a] = a;
+        if (isNaN(this._elements[b])) this._elements[b] = b;
+
         let rootA = findRoot(a, this._elements);
         let rootB = findRoot(b, this._elements);
 
-        if (this._numberElementsInComponent[a] < this._numberElementsInComponent[b]) {
+        if (isNaN(this._numberElementsInComponent[rootA])) this._numberElementsInComponent[rootA] = 1;
+        if (isNaN(this._numberElementsInComponent[rootB])) this._numberElementsInComponent[rootB] = 1;
 
-            this._elements[rootA] = rootB;
-            this._numberElementsInComponent[a] += this._numberElementsInComponent[b];
-        }
-        else {
-            this._elements[rootB] = rootA;
-            this._numberElementsInComponent[b] += this._numberElementsInComponent[a];
-        }
-        console.log('');
+
+        this._elements[rootA] = rootB;
+        this._numberElementsInComponent[rootB] += this._numberElementsInComponent[rootA];
 
     }
 
@@ -41,17 +32,19 @@ class QuickUnion {
         }
         let rootA = findRoot(a, this._elements);
         let rootB = findRoot(b, this._elements);
-        console.log('rootA ' + rootA);
-        console.log('rootB ' + rootB);
         return rootA == rootB;
     }
 
     count() {
-
+        let count = 0;
+        for (var nextElement = 0; nextElement < this._elements.length; nextElement++) {
+            if (this._elements[nextElement] == nextElement) count++;
+        }
+        return count;
     }
 
     component(a) {
-
+        return findRoot(a, this._elements);
     }
 
 }
--- test/quickUnion.spec.js
+++ test/quickUnion.spec.js
@@ -40,8 +40,8 @@ describe.only('#quikUnion', function () {
         let isElementConnected = qu.isConnected(4, 3);
         isElementConnected.should.be.eql(true);
     })
-    /*   it('#count should return number not only when we have 2 components', function () {
-     let uf = new algolib.UnionFind();
+       it('#count should return number not only when we have 2 components', function () {
+     let uf = new algolib.quickUnion();
      uf.connect(3, 6);
      uf.connect(8, 10);
      let numberOfComponents = uf.count();
@@ -49,14 +49,14 @@ describe.only('#quikUnion', function () {
      })
 
      it('#count should return number not only when we have 1 component', function () {
-     let uf = new algolib.UnionFind();
+     let uf = new algolib.quickUnion();
      uf.connect(3, 6);
      let numberOfComponents = uf.count();
      numberOfComponents.should.be.eql(1);
      })
 
      it('#count should return number not only when we have 3 components', function () {
-     let uf = new algolib.UnionFind();
+     let uf = new algolib.quickUnion();
      uf.connect(3, 6);
      uf.connect(7, 5);
      uf.connect(8, 2);
@@ -65,21 +65,21 @@ describe.only('#quikUnion', function () {
      })
 
      it('#component should return component name if component has 2 elements', function () {
-     let uf = new algolib.UnionFind();
+     let uf = new algolib.quickUnion();
      uf.connect(1, 2);
      let nameOfComponent = uf.component(1);
      nameOfComponent.should.be.eql(2);
      })
 
      it('#component should return undefined, because element not exist', function () {
-     let uf = new algolib.UnionFind();
+     let uf = new algolib.quickUnion();
      uf.connect(1, 2);
      let nameOfComponent = uf.component(5);
      (nameOfComponent === undefined).should.be.true()
      })
 
      it('#component should return component name if component has more than 3 elements', function () {
-     let uf = new algolib.UnionFind();
+     let uf = new algolib.quickUnion();
      uf.connect(1, 2);
      uf.connect(2, 3);
      uf.connect(1, 5);
@@ -88,10 +88,10 @@ describe.only('#quikUnion', function () {
      })
 
      it('#component should return component name if component has more than 2 elements', function () {
-     let uf = new algolib.UnionFind();
+     let uf = new algolib.quickUnion();
      uf.connect(1, 2);
      uf.connect(2, 3);
      let nameOfComponent = uf.component(2);
      nameOfComponent.should.be.eql(3);
-     })*/
+     })
 })
