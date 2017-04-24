1543cac32b5dd0e9afc92f269063878a378368cd
Vitaliy Normatov <vnormatov@gmail.com>
2017-04-24 22:35:28 +0300

    refactor

--- test/list-spec.js
+++ test/list-spec.js
@@ -3,103 +3,122 @@
 const algolib = require('../index.js');
 var assert = require('assert');
 
-describe.only('#list', function () {
+describe.only('LinkedList', function () {
 
-    it('#should create proper data structure', function () {
-        let list = new algolib.linkedList();
-        list.push.should.be.instanceOf(Function);
-        list.pop.should.be.instanceOf(Function);
-        list.shift.should.be.instanceOf(Function);
-        list.unshift.should.be.instanceOf(Function);
+    describe('#data structure', function () {
+
+        it('#should create proper data structure', function () {
+            let list = new algolib.linkedList();
+            list.push.should.be.instanceOf(Function);
+            list.pop.should.be.instanceOf(Function);
+            list.shift.should.be.instanceOf(Function);
+            list.unshift.should.be.instanceOf(Function);
+        });
+
+    });
+    describe('#push(element)', function () {
+
+        it('#push 1 element to list', function () {
+            let list = new algolib.linkedList();
+            list.push(12);
+            let element = list.pop();
+            element.should.be.eql(12);
+        });
+
+        it('#push 2 elements to list', function () {
+            let list = new algolib.linkedList();
+            list.push(12);
+            list.push(13);
+            let element = list.pop();
+            element.should.be.eql(13);
+        });
+
+        it('#push 3 elements to list', function () {
+            let list = new algolib.linkedList();
+            list.push(12);
+            list.push(13);
+            list.push(14);
+            let element = list.pop();
+            element.should.be.eql(14);
+        });
+
     });
 
-    it('#pop should throw exception if list is empty', function () {
-        let list = new algolib.linkedList();
-        assert.throws(list.pop, Error, 'List is empty');
+    describe('#pop()', function () {
+
+        it('#pop should throw exception if list is empty', function () {
+            let list = new algolib.linkedList();
+            assert.throws(list.pop, Error, 'List is empty');
+        });
+
+        it('#pop 2 elements from list', function () {
+            let list = new algolib.linkedList();
+            list.push(12);
+            list.push(13);
+            list.push(14);
+            list.pop();
+            let element = list.pop();
+            element.should.be.eql(13);
+        });
+
+        it('#pop all elements from list', function () {
+            let list = new algolib.linkedList();
+            list.push(12);
+            list.push(13);
+            list.push(14);
+            list.pop().should.be.eql(14);
+            list.pop().should.be.eql(13);
+            list.pop().should.be.eql(12);
+        });
+
     });
 
-    it('#push 1 element to list', function () {
-        let list = new algolib.linkedList();
-        list.push(12);
-        let element = list.pop();
-        element.should.be.eql(12);
+    describe('#shift(element) and #unshift()', function () {
+
+        it('#shift 3 elements to list', function () {
+            let list = new algolib.linkedList();
+            list.shift(12);
+            list.shift(13);
+            list.shift(14);
+            list.pop().should.be.eql(12);
+            list.pop().should.be.eql(13);
+            list.pop().should.be.eql(14);
+        });
+
+        it('#unshift 3 elements from list', function () {
+            let list = new algolib.linkedList();
+            list.shift(12);
+            list.shift(13);
+            list.shift(14);
+            list.unshift().should.be.eql(14);
+            list.unshift().should.be.eql(13);
+            list.unshift().should.be.eql(12);
+        });
+
     });
 
-    it('#push 2 elements to list', function () {
-        let list = new algolib.linkedList();
-        list.push(12);
-        list.push(13);
-        let element = list.pop();
-        element.should.be.eql(13);
-    });
+    describe('#all methods together', function () {
 
-    it('#push 3 elements to list', function () {
-        let list = new algolib.linkedList();
-        list.push(12);
-        list.push(13);
-        list.push(14);
-        let element = list.pop();
-        element.should.be.eql(14);
-    });
+        it('#push 3 elements and unshift 3 elements from the list', function () {
+            let list = new algolib.linkedList();
+            list.push(12);
+            list.push(13);
+            list.push(14);
+            list.unshift().should.be.eql(12);
+            list.unshift().should.be.eql(13);
+            list.unshift().should.be.eql(14);
+        });
 
-    it('#pop 2 elements from list', function () {
-        let list = new algolib.linkedList();
-        list.push(12);
-        list.push(13);
-        list.push(14);
-        list.pop();
-        let element = list.pop();
-        element.should.be.eql(13);
-    });
+        it('#shift 3 elements and pop 3 elements from the list', function () {
+            let list = new algolib.linkedList();
+            list.shift(12);
+            list.shift(13);
+            list.shift(14);
+            list.pop().should.be.eql(12);
+            list.pop().should.be.eql(13);
+            list.pop().should.be.eql(14);
+        });
 
-    it('#pop 3 elements from list', function () {
-        let list = new algolib.linkedList();
-        list.push(12);
-        list.push(13);
-        list.push(14);
-        list.pop().should.be.eql(14);
-        list.pop().should.be.eql(13);
-        list.pop().should.be.eql(12);
-    });
-
-    it('#shift 3 elements to list', function () {
-        let list = new algolib.linkedList();
-        list.shift(12);
-        list.shift(13);
-        list.shift(14);
-        list.pop().should.be.eql(12);
-        list.pop().should.be.eql(13);
-        list.pop().should.be.eql(14);
-    });
-
-    it('#unshift 3 elements from list', function () {
-        let list = new algolib.linkedList();
-        list.shift(12);
-        list.shift(13);
-        list.shift(14);
-        list.unshift().should.be.eql(14);
-        list.unshift().should.be.eql(13);
-        list.unshift().should.be.eql(12);
-    });
-
-    it('#push 3 elements and unshift 3 elements from the list', function () {
-        let list = new algolib.linkedList();
-        list.push(12);
-        list.push(13);
-        list.push(14);
-        list.unshift().should.be.eql(12);
-        list.unshift().should.be.eql(13);
-        list.unshift().should.be.eql(14);
-    });
-
-    it('#shift 3 elements and pop 3 elements from the list', function () {
-        let list = new algolib.linkedList();
-        list.shift(12);
-        list.shift(13);
-        list.shift(14);
-        list.pop().should.be.eql(12);
-        list.pop().should.be.eql(13);
-        list.pop().should.be.eql(14);
     });
 });
 
