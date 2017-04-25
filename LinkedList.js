f80f18ccfe6d6afd7fd1026e4a66e9cfe93f1a82
normatovv <normatovv@edi.su>
2017-04-25 10:57:01 +0300

    refactored

--- LinkedList.js
+++ LinkedList.js
@@ -4,9 +4,9 @@
 class ElementOfList {
 
     constructor(value, previous) {
-        this._elements = [];
-        this._elements['value'] = value;
-        this._elements['previous'] = previous;
+        this.elements = [];
+        this.elements['value'] = value;
+        this.elements['previous'] = previous;
     }
 
 }
@@ -14,61 +14,57 @@ class ElementOfList {
 class LinkedList {
 
     constructor() {
-        this.firstElement;
-        this.lastElement;
+        this._firstElement;
+        this._lastElement;
     }
 
-//add to end
-    push(element) {
+    push(value) {
         let elementOfList;
-        if (!(this.firstElement instanceof ElementOfList)) {
-            elementOfList = new ElementOfList(element, element);
-            this.firstElement = elementOfList;
-            this.lastElement = elementOfList;
+        if (!(this._firstElement instanceof ElementOfList)) {
+            elementOfList = new ElementOfList(value, value);
+            this._firstElement = elementOfList;
+            this._lastElement = elementOfList;
         }
         else {
-            elementOfList = new ElementOfList(element, this.lastElement);
-            this.lastElement._elements['next'] = elementOfList;
-            this.lastElement = elementOfList;
+            elementOfList = new ElementOfList(value, this._lastElement);
+            this._lastElement.elements['next'] = elementOfList;
+            this._lastElement = elementOfList;
         }
         return true;
     }
 
-//get and remove from end
     pop() {
-        if (!(this.lastElement instanceof ElementOfList)) {
+        if (!(this._lastElement instanceof ElementOfList)) {
             throw  new Error("List is empty");
         }
-        let element = this.lastElement._elements['value'];
-        this.lastElement = this.lastElement._elements['previous'];
+        let element = this._lastElement.elements['value'];
+        this._lastElement = this._lastElement.elements['previous'];
         return element;
     }
 
-    // add to start
-    shift(element) {
+    shift(value) {
         let elementOfList;
-        if (!(this.firstElement instanceof ElementOfList)) {
-            elementOfList = new ElementOfList(element, element);
-            this.firstElement = elementOfList;
-            this.lastElement = elementOfList;
+        if (!(this._firstElement instanceof ElementOfList)) {
+            elementOfList = new ElementOfList(value, value);
+            this._firstElement = elementOfList;
+            this._lastElement = elementOfList;
         }
         else {
-            elementOfList = new ElementOfList(element, this.lastElement);
-            let secondElement = this.firstElement;
-            this.firstElement = elementOfList;
-            secondElement._elements['previous'] = this.firstElement;
-            this.firstElement._elements['next'] = secondElement;
+            elementOfList = new ElementOfList(value, this._lastElement);
+            let secondElement = this._firstElement;
+            this._firstElement = elementOfList;
+            secondElement.elements['previous'] = this._firstElement;
+            this._firstElement.elements['next'] = secondElement;
         }
         return true;
     }
 
-    //get and remove from start
     unshift() {
-        if (!(this.firstElement instanceof ElementOfList)) {
+        if (!(this._firstElement instanceof ElementOfList)) {
             throw  new Error("List is empty");
         }
-        let element = this.firstElement._elements['value'];
-        this.firstElement = this.firstElement._elements['next'];
+        let element = this._firstElement.elements['value'];
+        this._firstElement = this._firstElement.elements['next'];
         return element;
     }
 }
