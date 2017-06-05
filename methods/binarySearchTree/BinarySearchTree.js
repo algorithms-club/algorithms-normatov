969e31b0b7affa2069acb5392b206166f8623475
Vitaliy Normatov <vnormatov@gmail.com>
2017-06-05 20:41:04 +0300

    added getCount and tests

--- methods/binarySearchTree/BinarySearchTree.js
+++ methods/binarySearchTree/BinarySearchTree.js
@@ -7,30 +7,41 @@ class BinarySearchTree {
             left: 'undefined',
             right: 'undefined',
             value: value,
+            count: 0,
         }
     }
 
     push(value) {
+        if(value==this._element.value) return;
         if (this._element.value === undefined && this._element.left == 'undefined' && this._element.right == 'undefined') {
+            this._element.count++;
             this._element.value = value;
             return;
         }
 
         if (this._element.left == 'undefined' && value < this._element.value) {
             this._element.left = new BinarySearchTree(value);
+            this._element.count++;
+            return;
         }
 
-        if (this._element.left != 'undefined' && value < this._element.value ) {
+        if (this._element.left != 'undefined' && value < this._element.value) {
             this._element.left.push(value);
+            this._element.count++;
+            return;
         }
 
 
         if (this._element.right == 'undefined' && value > this._element.value) {
             this._element.right = new BinarySearchTree(value);
+            this._element.count++;
+            return;
         }
 
-        if (this._element.right != 'undefined' && value > this._element.value ) {
+        if (this._element.right != 'undefined' && value > this._element.value) {
             this._element.right.push(value);
+            this._element.count++;
+            return;
         }
 
     }
@@ -40,7 +51,7 @@ class BinarySearchTree {
             return this._element.value;
         }
         else {
-           return this._element.left.getMin();
+            return this._element.left.getMin();
         }
     }
 
@@ -51,6 +62,10 @@ class BinarySearchTree {
         else {
             return this._element.right.getMax();
         }
+    }
+
+    getCount() {
+        return this._element.count;
     }
 
 }
--- test/BinarySearchTree-spec.js
+++ test/BinarySearchTree-spec.js
@@ -3,7 +3,7 @@
 const algolib = require('../index.js');
 var assert = require('assert');
 
-describe('BinarySearch', function () {
+describe.only('BinarySearchTree', function () {
     describe('#data structure', function () {
         it('#should create proper data structure', function () {
             let bs = new algolib.BinarySearchTree();
@@ -19,6 +19,15 @@ describe('BinarySearch', function () {
             bs.getMax().should.be.eql(1);
             bs.getMin().should.be.eql(1);
         });
+        it('#should push same elements to tree', function () {
+            let bs = new algolib.BinarySearchTree();
+            bs.push(1);
+            bs.push(1);
+            bs.push(1);
+            bs.push(1);
+            bs.getMax().should.be.eql(1);
+            bs.getMin().should.be.eql(1);
+        });
         it('#should push three elements to tree', function () {
             let bs = new algolib.BinarySearchTree();
             bs.push(2);
@@ -27,20 +36,56 @@ describe('BinarySearch', function () {
             bs.getMin().should.be.eql(1);
             bs.getMax().should.be.eql(3);
         });
-        it.only('#should push ten elements to tree', function () {
+        it('#should push ten elements to tree', function () {
+            let bs = new algolib.BinarySearchTree();
+            bs.push(2);
+            bs.getMin().should.be.eql(2);
+            bs.getMax().should.be.eql(2);
+            bs.push(1);
+            bs.getMax().should.be.eql(2);
+            bs.getMin().should.be.eql(1);
+            bs.push(3);
+            bs.getMin().should.be.eql(1);
+            bs.getMax().should.be.eql(3);
+            bs.push(4);
+            bs.getMax().should.be.eql(4);
+            bs.getMin().should.be.eql(1);
+            bs.push(-1);
+            bs.getMax().should.be.eql(4);
+            bs.getMin().should.be.eql(-1);
+            bs.push(-3);
+            bs.getMax().should.be.eql(4);
+            bs.getMin().should.be.eql(-3);
+            bs.push(-5);
+            bs.getMax().should.be.eql(4);
+            bs.getMin().should.be.eql(-5);
+            bs.push(5);
+            bs.getMax().should.be.eql(5);
+            bs.getMin().should.be.eql(-5);
+            bs.push(15);
+            bs.getMax().should.be.eql(15);
+            bs.getMin().should.be.eql(-5);
+            bs.push(10);
+            bs.getMin().should.be.eql(-5);
+            bs.getMax().should.be.eql(15);
+        });
+    });
+    describe('#getCount function', function () {
+        it('#should return count of nods, if  tree root has 2 nods', function () {
+            let bs = new algolib.BinarySearchTree();
+            bs.push(2);
+            bs.push(1);
+            bs.push(3);
+            bs.getCount().should.eql(3);
+        });
+        it('#should return count of nods, if  tree root has 5  nods', function () {
             let bs = new algolib.BinarySearchTree();
             bs.push(2);
             bs.push(1);
             bs.push(3);
             bs.push(4);
-            bs.push(-1);
-            bs.push(-3);
-            bs.push(-5);
             bs.push(5);
-            bs.push(15);
-            bs.push(10);
-            bs.getMin().should.be.eql(-5);
-            bs.getMax().should.be.eql(15);
+            bs.getCount().should.eql(5);
         });
     });
 });
